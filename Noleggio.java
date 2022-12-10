be737e913838cdaf21223fd0f2c1428ee9ffd086
ZiniFili <101247036+Zifili@users.noreply.github.com>
2022-12-10 15:48:38 +0100

    gestione bici e rack

--- Noleggio.java
+++ Noleggio.java
@@ -1,3 +1,10 @@
 public class Noleggio {
-    
+    private long initDate,endDate;
+    private String targa;
+    private SmartCard SM;
+    private String initSt,endSt;
+
+    public Noleggio(long initDate,String targa,SmartCard SM,String initSt){
+        
+    }
 }
--- Sistema.java
+++ Sistema.java
@@ -4,12 +4,16 @@ public class Sistema {
     private SmartCard[] sm;
     private Rack[] racks;
     private String comune;
+    private int biciTot,rackTot;
+    private int biciNow = 0,rackNow = 0;
 
-    public Sistema(String comune){
-        bici =  new Bici[100];
-        noleggi = new Noleggio[100];
-        sm = new SmartCard[100];
-        racks = new Rack[100];
+    public Sistema(String comune,int biciTot, int rackTot){ //biciTot indica quanti bici può avere al massimo il comune
+        this.biciTot = biciTot;                             //rackTot indica quante rack può avere al massimo il comune
+        this.rackTot = rackTot;
+        bici =  new Bici[biciTot];
+        noleggi = new Noleggio[1000];
+        sm = new SmartCard[1000];
+        racks = new Rack[rackTot];
         this.comune = comune;
     }
 
@@ -27,5 +31,20 @@ public class Sistema {
     public void addRack(){
         //serve al sistema per aggiungere racks
     }
-
+    public void ricaricaSM(int importo, SmartCard sm){
+        //aggiorna credito Sm
+    }
+    public void chechValidity(){}
+    public void createBicis(Bici[] b, int totB){
+        for(int i = biciNow; i<totB; i++){
+            b[i] = new Bici("b"+biciNow);
+            biciNow++;
+        }
+    }
+    public void createRacks(Rack[] r, int totR,int totP){
+        for(int i = rackNow; i<totR; i++){
+            r[i] = new Rack(rackNow,totP);
+            rackNow++;
+        }
+    }
 }
