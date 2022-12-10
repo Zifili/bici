a3045d9c7da304e191e90d1e17e58dcf53b170fb
ZiniFili <101247036+Zifili@users.noreply.github.com>
2022-12-10 19:22:37 +0100

    gay

--- Noleggio.java
+++ Noleggio.java
@@ -13,12 +13,14 @@ public class Noleggio {
         this.initSt = initSt.getCodice();
     }
     public void setEndSt(String endSt){
+        //check if endSt exists
         this.endSt = endSt;
     }
     public GregorianCalendar getEndDate(){
         return endDate;
     } 
     public void setEndDate(int g, int m, int a){
+        //check if endDate is a valid date
         endDate = new GregorianCalendar(a,m,g);
     }
     public String getTarga() {
