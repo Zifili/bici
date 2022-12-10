559cc905c829378e3d5b011adefe7cdb4842d940
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2022-12-10 15:57:39 +0100

    Update SmartCard.java

--- SmartCard.java
+++ SmartCard.java
@@ -5,6 +5,13 @@ public class SmartCard {
     Date attivazione;
     int credito=0;
     String nominativo;
+
+    public SmartCard(String codice, Date attivazione, int credito, String nominativo) {
+        this.codice = codice;
+        this.attivazione = attivazione;
+        this.credito = credito;
+        this.nominativo = nominativo;
+    }
     public String getCodice() {
         return codice;
     }
